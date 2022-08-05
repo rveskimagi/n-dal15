bc855ba43f249c92663864d92e91ee5397e5e544
rvesk <jksdgn@gmail.com>
2022-08-05 09:21:04 +0300

    add new procjects

---
+++ Frozen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayElementsFromArray(GetDataFromFile());
+        }
+
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\rvesk\samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
---
+++ OOPAndFiles/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOPAndFiles
+{
+    class Program
+    {
+        class Movie
+        {
+            string title;
+            string rating;
+            string year;
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+            public string Title
+            {
+                get { return title; }
+            }
+
+            public string Rating
+            {
+                get { return rating; }
+            }
+
+            public string Year
+            { 
+                get { return year; }
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            List<Movie> myMovies = new List<Movie>();
+            string[] moviesFromFile = GetDataFromFIle();
+
+            foreach(string line in moviesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions. RemoveEmptyEntries);
+                Console.WriteLine($"title from TempArray: {tempArray[0]}");
+                Console.WriteLine($"rating from TempArray: {tempArray[1]}");
+                Console.WriteLine($"year from TempArray: {tempArray[2]}");
+                Console.WriteLine();
+            }
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFIle()
+        {
+            string filePath = @"C:\Users\rvesk\samples\movies.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+
+        }
+
+
+    }
+
+
+    
+}
